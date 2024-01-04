1cb5b1bf78c6dbc29341506166366b23ec07ca2f
cucumber99 <dlsguq1@gmail.com>
2024-01-05 00:25:54 +0900

    feat: Navigation Bar 공통 위젯 추가

---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+
+class NavBarWidget extends StatelessWidget {
+  const NavBarWidget({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house, size: 15.0),
+            label: 'Main',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.calendar, size: 15.0),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.youtube, size: 15.0),
+            label: 'Youtube',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.user, size: 15.0),
+            label: 'Information',
+          ),
+        ],
+        onTap: (index) => _onItemTapped(index, context),
+        currentIndex: _calculateIndex(context),
+      ),
+    );
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        GoRouter.of(context).go('/main');
+        break;
+      case 1:
+        GoRouter.of(context).go('/calendar');
+        break;
+      case 2:
+        GoRouter.of(context).go('/youtube');
+        break;
+      case 3:
+        GoRouter.of(context).go('/information');
+        break;
+    }
+  }
+
+  int _calculateIndex(BuildContext context) {
+    final currentLocation = GoRouterState.of(context).uri.toString();
+    if (currentLocation == '/main') {
+      return 0;
+    }
+    if (currentLocation == '/calendar') {
+      return 1;
+    }
+    if (currentLocation == '/youtube') {
+      return 2;
+    }
+    if (currentLocation == '/information') {
+      return 3;
+    }
+    return 0;
+  }
+}
