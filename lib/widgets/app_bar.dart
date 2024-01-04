63430ede69abd7dc88edf60fb2a3c786caebfef6
cucumber99 <dlsguq1@gmail.com>
2024-01-05 00:25:25 +0900

    feat: AppBar 공통 위젯 추가

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+PreferredSizeWidget customAppBar(BuildContext context) {
+  return AppBar(
+    leading: Image.asset("assets/images/logo.png", fit: BoxFit.contain),
+    backgroundColor: Colors.white,
+    actions: <Widget>[
+      IconButton(
+        icon: const FaIcon(FontAwesomeIcons.bell),
+        onPressed: () {},
+      ),
+      Builder(
+        builder: (context) => IconButton(
+          icon: const FaIcon(FontAwesomeIcons.bars),
+          onPressed: () => Scaffold.of(context).openEndDrawer(),
+        ),
+      ),
+    ],
+  );
+}
