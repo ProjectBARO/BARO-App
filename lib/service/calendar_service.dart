f3f6fa8effc78155b6b95931bff7e5c96b16b56c
cucumber99 <dlsguq1@gmail.com>
2024-01-26 15:47:42 +0900

    [#11] feat: 월 단위 레포트 조회 기능 추가

---
+++ lib/service/calendar_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:baro_project/models/calendar.dart';
+
+class CalendarService {
+  final storage = const FlutterSecureStorage();
+
+  Future<List<Calendar>?> getCalendars(int date) async {
+    String? accessToken = await storage.read(key: 'accessToken');
+    var response = await http.get(Uri.parse('${dotenv.get('SERVER_URL')}/analysis/summary?ym=${date.toString()}'),
+        headers: {'accept': 'application/json', 'Authorization': 'Bearer $accessToken'});
+    if (response.statusCode == 200) {
+      var body = jsonDecode(response.body);
+      if (body['data'] != null) {
+        log('레포트 데이터 로드 성공');
+        List<Calendar> calendars =
+            (body['data'] as List).map((i) => Calendar.fromJson(i as Map<String, dynamic>)).toList();
+        return calendars;
+      } else {
+        log('데이터가 존재하지 않습니다.');
+      }
+    } else {
+      log('레포트 데이터 로드 실패: ${response.statusCode}');
+      return null;
+    }
+    return null;
+  }
+}
