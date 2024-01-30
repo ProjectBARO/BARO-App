3ff05335b813cd4f2f766565338c0523e4ed0df0
cucumber99 <dlsguq1@gmail.com>
2024-01-30 09:44:38 +0900

    [#12] feat: 유저 모델 및 프로바이더 수정

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,25 +1,39 @@
 class User {
   final String? email;
   final String? name;
-  // final String? photoUrl;
+  final String? nickname;
   final int? birthday;
   final String? gender;
 
-  User({
-    required this.email,
-    required this.name,
-    // required this.photoUrl,
-    required this.birthday,
-    required this.gender
-  });
+  User({required this.email, required this.name, this.nickname, required this.birthday, required this.gender});
 
   Map<String, dynamic> toJson() {
     return {
       'email': email,
-      'name' : name,
-      // 'photoUrl': photoUrl,
+      'name': name,
+      'nickname': nickname,
       'birthday': birthday,
       'gender': gender,
     };
+  }
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      email: json['email'] as String?,
+      name: json['name'] as String?,
+      nickname: json['nickname'] as String?,
+      birthday: json['birthday'] as int?,
+      gender: json['gender'] as String?,
+    );
+  }
+
+  User copyWith({String? email, String? name, String? nickname, int? birthday, String? gender}) {
+    return User(
+      email: email ?? this.email,
+      name: name ?? this.name,
+      nickname: nickname ?? this.nickname,
+      birthday: birthday ?? this.birthday,
+      gender: gender ?? this.gender,
+    );
   }
 }
