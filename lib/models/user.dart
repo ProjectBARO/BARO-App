e9fb3f5268cfcf366c5d37d0f4681f45337326b9
cucumber99 <dlsguq1@gmail.com>
2024-02-06 16:41:37 +0900

    [#19] refactor: 필드명 수정

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,17 +2,17 @@ class User {
   final String? email;
   final String? name;
   final String? nickname;
-  final int? birthday;
+  final int? age;
   final String? gender;
 
-  User({required this.email, required this.name, this.nickname, required this.birthday, required this.gender});
+  User({required this.email, required this.name, this.nickname, required this.age, required this.gender});
 
   Map<String, dynamic> toJson() {
     return {
       'email': email,
       'name': name,
       'nickname': nickname,
-      'birthday': birthday,
+      'age': age,
       'gender': gender,
     };
   }
@@ -22,17 +22,17 @@ class User {
       email: json['email'] as String?,
       name: json['name'] as String?,
       nickname: json['nickname'] as String?,
-      birthday: json['birthday'] as int?,
+      age: json['age'] as int?,
       gender: json['gender'] as String?,
     );
   }
 
-  User copyWith({String? email, String? name, String? nickname, int? birthday, String? gender}) {
+  User copyWith({String? email, String? name, String? nickname, int? age, String? gender}) {
     return User(
       email: email ?? this.email,
       name: name ?? this.name,
       nickname: nickname ?? this.nickname,
-      birthday: birthday ?? this.birthday,
+      age: age ?? this.age,
       gender: gender ?? this.gender,
     );
   }
