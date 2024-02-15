8c1bafb89d09887629342669d3a77b5b8a304ecf
cucumber99 <dlsguq1@gmail.com>
2024-02-15 23:11:24 +0900

    [#17] feat: 동영상 위젯 추가

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,17 +1,12 @@
 import 'package:baro_project/widgets/percentage_view.dart';
 import 'package:baro_project/widgets/tip_text.dart';
+import 'package:baro_project/widgets/video_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class MainScreen extends ConsumerStatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
-  @override
-  MainScreenState createState() => MainScreenState();
-}
-
-class MainScreenState extends ConsumerState<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +19,8 @@ class MainScreenState extends ConsumerState<MainScreen> {
                     child: Text(
                       "BARO와 함께\n공부, 작업 등 앉아있는 시간동안\n자세를 측정해보세요!", 
                       textAlign: TextAlign.center, 
-                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0),),
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20.0)
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () => context.push('/category'),
@@ -37,52 +33,74 @@ class MainScreenState extends ConsumerState<MainScreen> {
                     padding: const EdgeInsets.only(top: 20.0),
                     child: SizedBox(
                       width: MediaQuery.of(context).size.width*0.8,
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height*0.15),
-                        child: DecoratedBox(
-                          decoration: const BoxDecoration(color: Color(0xffDAEDFF), borderRadius: BorderRadius.all(Radius.circular(20))),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 10.0),
-                                child: Container(
-                                  alignment: Alignment.topLeft,
-                                  child: const Text("💡 오늘의 팁", style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w500)),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-                                child: Container(
-                                  alignment: Alignment.topLeft,
-                                  child: randomTip(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width*0.8,
                       height: MediaQuery.of(context).size.height*0.3,
                       child: const DecoratedBox(
                         decoration: BoxDecoration(color: Color(0xffDAEDFF), borderRadius: BorderRadius.all(Radius.circular(20))),
                         child: PercentageView(),
                       ),
                     ),
-                  ),                  
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width*0.8,
-                    height: 300,
-                    child: const DecoratedBox(
-                      decoration: BoxDecoration(color: Color(0xffDAEDFF), borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Stack(
+                        alignment: Alignment.topLeft,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 30.0),
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                  minHeight: MediaQuery.of(context).size.height * 0.1),
+                              child: DecoratedBox(
+                                decoration: const BoxDecoration(
+                                    color: Color(0xffDAEDFF),
+                                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0, right: 20.0, bottom: 20.0, top: 20.0),
+                                  child: Container(
+                                    alignment: Alignment.topLeft,
+                                    child: randomTip(),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            top: 0,
+                            child: Text(
+                              "💡 바른 자세 팁",
+                              style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 20.0)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 50.0),
+                          child: Text("스트레칭 맞춤 영상", style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w500)),
+                        ),
+                        GestureDetector(
+                          onTap: () => context.go('/youtube'),
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * 0.2,
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 15.0),
+                              child: VideoPreview()
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               )
           )
---
+++ lib/widgets/video_preview.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+import 'package:baro_project/provider/youtube_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class VideoPreview extends ConsumerWidget {
+  const VideoPreview({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(youtubeProvider).when(
+      data: (videos) {
+        return ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                    child: Image.network(videos[index].thumbnailUrl),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stackTrace) {
+        log(error.toString());
+        log(stackTrace.toString());
+        return const Center(child: Text("동영상을 불러오는데 실패했습니다."));
+      }
+    );
+  }
+}
