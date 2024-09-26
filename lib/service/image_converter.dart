a074cbe6e17492ab8eb387dbe12b1e356439985c
cucumber99 <dlsguq1@gmail.com>
2024-09-27 00:27:28 +0900

    [#24] refactor: 이미지 전처리 로직 개선

--- lib/service/image_converter.dart
+++ lib/service/image_converter.dart
@@ -5,13 +5,14 @@ class ImageConverter {
   static const int INPUT_SIZE = 28;
   static Float32List convertImage(Uint8List img) {
     imagelib.Image? image = imagelib.decodeImage(img);
-    imagelib.Image rgbImage = imagelib.copyResize(image!, width: INPUT_SIZE, height: INPUT_SIZE);
+    imagelib.Image resizedImage = imagelib.copyResize(image!, width: INPUT_SIZE, height: INPUT_SIZE);
 
     var imgData = Float32List(INPUT_SIZE * INPUT_SIZE * 3);
     int index = 0;
+
     for (var x = 0; x < INPUT_SIZE; x++) {
       for (var y = 0; y < INPUT_SIZE; y++) {
-        var pixel = rgbImage.getPixelSafe(x, y);
+        var pixel = resizedImage.getPixel(x, y);
         imgData[index++] = pixel.r / 255.0; // Red
         imgData[index++] = pixel.g / 255.0; // Green
         imgData[index++] = pixel.b / 255.0; // Blue
